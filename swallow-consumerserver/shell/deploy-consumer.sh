680f34f43d403410440b10d7259f938a8a7c13ae
kezhu.wu <atell.wu@gmail.com>
2013-04-26 23:39:57 +0800

    完善shell脚本

--- swallow-consumerserver/shell/deploy-consumer.sh
+++ swallow-consumerserver/shell/deploy-consumer.sh
@@ -1,23 +1,42 @@
 #!/bin/bash
 ##########################################################################################################################################
 #  dev的consumerserver部署方法：
-#  (1)打包tar之后，放到ftp，路径为ftp://10.1.1.189/arch/<当天日期>/swallow-consumerserver-withDependencies.tar
-#  (2)到192.168.8.21机器的/home/wukezhu/目录下，执行sh deploy-consumer-dev.sh即可。
+#  (1)打包tar之后，放到ftp，路径为ftp://10.1.1.189/arch/<当天日期>/<Mode>/swallow-consumerserver-withDependencies.tar
+#  (2)到相应机器(dev/alpha/beta)的 $AppRootDir 目录下，执行sh deploy-consumer.sh <Mode>即可。
 #
 ##########################################################################################################################################
+
+
+##################################################
+# Some utility functions
+##################################################
+usage(){
+    echo "  Usage:"
+    echo "     use '$0 dev/alpha/qa'."
+    exit 1
+}
+pause(){
+   read -p "$*"
+}
+
+#环境变量
 source /etc/profile
 FtpServer="ftp://10.1.1.189/arch"
 Datedir=$(date +%Y%m%d)
 TarFile="swallow-consumerserver-withDependencies.tar"
-Mode="dev"
+Mode=$1
 FileFtpAddress="$FtpServer/$Datedir/$Mode/$TarFile"
-UserDir="/home/wukezhu"
+if [ "$Mode" = "dev" ]; then
+    UserDir="/home/wukezhu"
+elif [ "$Mode" = "alpha" -o "$Mode" = "qa" ]; then
+    UserDir="/data/swallow"
+else
+    echo "Your input is not corrent!"
+    usage
+    exit 1
+fi
 MasterDir="swallow-consumerserver-master"
 SlaveDir="swallow-consumerserver-slave"
-
-function pause(){
-   read -p "$*"
-}
 
 cd $UserDir
 
@@ -34,14 +53,14 @@ rm -rf $SlaveDir && mkdir $SlaveDir && tar xf $TarFile -C $SlaveDir
 echo "Backuped master dir."
 
 #重启master
-echo $
+echo ""
 echo "========== To restart master =========="
 pause 'Press [Enter] key to continue...'
 cd $UserDir/$MasterDir
 sh swallow.sh restart master
 
 #重启slave
-echo $
+echo ""
 echo "========== To restart slave =========="
 pause 'Press [Enter] key to continue...'
 cd $UserDir/$SlaveDir
--- swallow-producerserver/shell/deploy-producer.sh
+++ swallow-producerserver/shell/deploy-producer.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 ##########################################################################################################################################
 #  dev的producerserver部署方法：
-#  (1)打包tar之后，放到ftp，路径为ftp://10.1.1.189/arch/<当天日期>/swallow-producerserver-withDependencies.tar
-#  (2)到192.168.8.22机器的/home/wukezhu/目录下，执行sh deploy-producer-dev.sh即可。
+#  (1)打包tar之后，放到ftp，路径为ftp://10.1.1.189/arch/<当天日期>/<Mode>/swallow-producerserver-withDependencies.tar
+#  (2)到相应机器(dev/alpha/beta)的 $AppRootDir 目录下，执行sh deploy-producer.sh <Mode>即可。
 #
 ##########################################################################################################################################
+
+##################################################
+# Some utility functions
+##################################################
+usage(){
+    echo "  Usage:"
+    echo "     use '$0 dev/alpha/qa'."
+    exit 1
+}
+pause(){
+   read -p "$*"
+}
+
+#环境变量
 source /etc/profile
 FtpServer="ftp://10.1.1.189/arch"
 Datedir=$(date +%Y%m%d)
 TarFile="swallow-producerserver-withDependencies.tar"
-Mode="dev"
+Mode=$1
 FileFtpAddress="$FtpServer/$Datedir/$Mode/$TarFile"
-UserDir="/home/wukezhu"
+if [ "$Mode" = "dev" ]; then
+    UserDir="/home/wukezhu"
+elif [ "$Mode" = "alpha" -o "$Mode" = "qa" ]; then
+    UserDir="/data/swallow"
+else
+    echo "Your input is not corrent!"
+    usage
+    exit 1
+fi
 AppDir="swallow-producerserver"
-
-function pause(){
-   read -p "$*"
-}
 
 cd $UserDir
 
@@ -30,7 +48,7 @@ rm -rf $AppDir && mkdir $AppDir && tar xf $TarFile -C $AppDir
 echo "Backuped producer dir."
 
 #重启
-echo $
+echo ""
 echo "========== To restart =========="
 pause 'Press [Enter] key to continue...'
 cd $UserDir/$AppDir
