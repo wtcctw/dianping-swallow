aca2651b4f24ab6962ecad227a1b481998086b21
kezhu.wu <atell.wu@gmail.com>
2013-05-02 21:53:28 +0800

    完善jvm-stat脚本

--- swallow-producerserver/src/main/resources/jvm-stat.sh
+++ swallow-producerserver/src/main/resources/jvm-stat.sh
@@ -8,21 +8,22 @@ fi
 jvmStat(){
   Pid=$1
   ProcessName=$2
-  echo "Now jstacking $ProcessName(pid=$Pid) to $StatDir/$ProcessName.jstack.out..."
+  echo -e "======= Executing jstack \033[32m$ProcessName\033[0m(pid=$Pid) to $StatDir/$ProcessName.jstack.out..."
   jstack $Pid > $StatDir/$ProcessName.jstack.out
-  echo "Now jmap -heap $ProcessName(pid=$Pid) to $StatDir/$ProcessName.jmap-heap.out..."
+  echo -e "======= Executing jmap -heap \033[32m$ProcessName\033[0m(pid=$Pid) to $StatDir/$ProcessName.jmap-heap.out..."
   jmap -heap $Pid > $StatDir/$ProcessName.jmap-heap.out
-  echo "Now jmap -histo $ProcessName(pid=$Pid) to $StatDir/$ProcessName.jmap-histo.out ..."
+  echo -e "======= Executing jmap -histo \033[32m$ProcessName\033[0m(pid=$Pid) to $StatDir/$ProcessName.jmap-histo.out ..."
   jmap -histo $Pid > $StatDir/$ProcessName.jmap-histo.out
 }
 
 for i in MasterBootStrap SlaveBootStrap ProducerServerBootstrap
   do
- PidCount=$(jps |grep $i|wc -l)
- if [ $PidCount -ge 1 ]; then
-   Pid=$(jps |grep $i |cut -d\  -f1)
-   echo "finded Process name $i, pid is $Pid"
-   ProcessName=$i
-   jvmStat $Pid $ProcessName
- fi
+   PidCount=$(jps |grep $i|wc -l)
+   if [ $PidCount -ge 1 ]; then
+     Pid=$(jps |grep $i |cut -d\  -f1)
+     echo -e "======= finded Process name \033[32m$i\033[0m, pid is $Pid"
+     ProcessName=$i
+     jvmStat $Pid $ProcessName
+     echo ''
+   fi
 done
