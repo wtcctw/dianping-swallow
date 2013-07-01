56f01f914c5a92b926024dc32672bc798e4e5654
atellwu <atell@qq.com>
2013-07-01 14:56:05 +0800

    init swallow-broker

---
+++ swallow-broker/src/main/java/com/dianping/swallow/broker/web/BrokerController.java
@@ -0,0 +1,37 @@
+package com.dianping.swallow.broker.web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.dianping.swallow.broker.util.GsonUtil;
+
+@Controller
+public class BrokerController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BrokerController.class);
+
+    @RequestMapping(value = "/sendMsg", method = { RequestMethod.POST, RequestMethod.GET }, produces = "application/json; charset=utf-8")
+    @ResponseBody
+    public Object sendMsg(String topic, String oType) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        try {
+            map.put("success", true);
+        } catch (IllegalArgumentException e) {
+            map.put("success", false);
+            map.put("errorMsg", e.getMessage());
+        } catch (Exception e) {
+            map.put("success", false);
+            map.put("errorMsg", e.getMessage());
+            LOG.error(e.getMessage(), e);
+        }
+        return GsonUtil.toJson(map);
+    }
+
+}
