f13852c463f4b73c4b9f146e0cd220cd1e030331
atellwu <atell@qq.com>
2013-07-03 11:55:40 +0800

    完成producer接口

--- swallow-broker/src/main/java/com/dianping/swallow/broker/service/impl/ProducerHolderImpl.java
+++ swallow-broker/src/main/java/com/dianping/swallow/broker/service/impl/ProducerHolderImpl.java
@@ -4,12 +4,15 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.dianping.swallow.broker.monitor.NotifyService;
 import com.dianping.swallow.broker.service.ProducerHolder;
 import com.dianping.swallow.common.internal.config.ConfigChangeListener;
 import com.dianping.swallow.common.internal.config.DynamicConfig;
@@ -22,6 +25,8 @@ import com.dianping.swallow.producer.impl.ProducerFactoryImpl;
 
 @Service
 public class ProducerHolderImpl implements ProducerHolder, ConfigChangeListener {
+    private static final String   TOPIC       = "topic";
+
     private static final Logger   LOG         = LoggerFactory.getLogger(ProducerHolderImpl.class);
 
     private Map<String, Producer> producerMap = new ConcurrentHashMap<String, Producer>();
@@ -29,24 +34,36 @@ public class ProducerHolderImpl implements ProducerHolder, ConfigChangeListener 
     @Autowired
     private DynamicConfig         dynamicConfig;
 
+    @Autowired
+    private NotifyService         notifyService;
+
+    @PostConstruct
     public void init() throws RemoteServiceInitFailedException {
         //<topic>,<topic>
-        String topicStr = dynamicConfig.get("topic");
-        String[] topics = StringUtils.split(topicStr, ',');
-        LOG.info("initing producers with topics(" + Arrays.toString(topics) + ")");
+        String topicStr = dynamicConfig.get(TOPIC);
+        String[] topics = StringUtils.split(topicStr, ';');
+        LOG.info("Initing producers with topics(" + Arrays.toString(topics) + ")");
 
         //每个topic创建一个producer(生产者是可以并发使用的)
         if (topics != null) {
             for (String topic : topics) {
-                ProducerConfig config = new ProducerConfig();
-                config.setMode(ProducerMode.SYNC_MODE);
-                Producer producer = ProducerFactoryImpl.getInstance().createProducer(Destination.topic(topic), config);
-                producerMap.put(topic, producer);
+                initializeProducer(topic);
             }
         }
 
         //监听lion
         dynamicConfig.addConfigChangeListener(this);
+
+        LOG.info("Producer map" + producerMap);
+    }
+
+    private void initializeProducer(String topic) throws RemoteServiceInitFailedException {
+        ProducerConfig config = new ProducerConfig();
+        config.setMode(ProducerMode.SYNC_MODE);
+        Producer producer = ProducerFactoryImpl.getInstance().createProducer(Destination.topic(topic), config);
+        if (!producerMap.containsKey(topic)) {//不存在该topic的producer时，才会添加改Producer
+            producerMap.put(topic, producer);
+        }
     }
 
     @Override
@@ -56,8 +73,23 @@ public class ProducerHolderImpl implements ProducerHolder, ConfigChangeListener 
 
     @Override
     public void onConfigChange(String key, String value) {
-        // TODO Auto-generated method stub
+        if (StringUtils.equals(key, TOPIC)) {
+            String[] topics = StringUtils.split(value, ';');
+            LOG.info("Initing producers with topics(" + Arrays.toString(topics) + ")");
 
+            //每个topic创建一个producer(生产者是可以并发使用的)
+            if (topics != null) {
+                for (String topic : topics) {
+                    try {
+                        initializeProducer(topic);
+                    } catch (RemoteServiceInitFailedException e) {
+                        notifyService.alarm("Error initialize producer ", e, true);
+                    }
+                }
+            }
+
+            LOG.info("Producer map" + producerMap);
+        }
     }
 
 }
--- swallow-broker/src/main/java/com/dianping/swallow/broker/web/BrokerController.java
+++ swallow-broker/src/main/java/com/dianping/swallow/broker/web/BrokerController.java
@@ -1,13 +1,11 @@
 package com.dianping.swallow.broker.web;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,7 +17,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.dianping.swallow.broker.service.ProducerHolder;
 import com.dianping.swallow.broker.util.GsonUtil;
-import com.dianping.swallow.broker.vo.ParamVO;
 import com.dianping.swallow.producer.Producer;
 
 @Controller
@@ -35,7 +32,7 @@ public class BrokerController {
     private ProducerHolder      producerHolder;
 
     @SuppressWarnings({ "rawtypes", "unchecked" })
-    @RequestMapping(value = "/sendMsg", method = { RequestMethod.POST, RequestMethod.GET }, produces = "application/json; charset=utf-8")
+    @RequestMapping(method = { RequestMethod.POST, RequestMethod.GET }, produces = "application/json; charset=utf-8")
     @ResponseBody
     public Object sendMsg(HttpServletRequest request) {
         Map<String, Object> map = new HashMap<String, Object>();
@@ -48,6 +45,7 @@ public class BrokerController {
             //自定义的参数
             Map<String, String> properties = new HashMap<String, String>();
 
+            //解析请球中的参数
             Map paramMap = request.getParameterMap();
             LOG.info("request param is " + paramMap);
 
@@ -55,22 +53,35 @@ public class BrokerController {
                 Set<Map.Entry> set = paramMap.entrySet();
                 for (Map.Entry paramEntry : set) {
                     String name = (String) paramEntry.getKey();
-                    String value = (String) paramEntry.getValue();
-                    if (CONTENT.equals(name)) {
-                        content = value;
-                    } else if (TOPIC.equals(name)) {
-                        topic = value;
-                    } else {
-                        properties.put(name, value);
+                    String[] values = (String[]) paramEntry.getValue();
+                    if (values != null && values.length > 0) {
+                        String value = values[0].toString();
+                        if (CONTENT.equals(name)) {
+                            content = value;
+                        } else if (TOPIC.equals(name)) {
+                            topic = value;
+                        } else {
+                            properties.put(name, value);
+                        }
                     }
                 }
             }
+            //验证参数
+            if (StringUtils.isBlank(topic)) {
+                throw new IllegalArgumentException("Topic (" + topic + ") can not be blank.");
+            } else if (StringUtils.isEmpty(content)) {
+                throw new IllegalArgumentException("Content (" + content + ") can not be empty.");
+            }
 
-            //根据topic找到Producer，进行发送
+            //根据topic找到Producer
+            LOG.info("Sending message: topic=" + topic + ", content=" + content + ", properties=" + properties);
             Producer producer = producerHolder.getProducer(topic);
-
-            //            String messageContent = "o" + URLEncoder.encode(oType, "UTF-8") + "&";
-            producer.sendMessage(content, properties);
+            if (producer != null) {
+                //发送消息
+                producer.sendMessage(content, properties);
+            } else {
+                throw new IllegalArgumentException("Topic '" + topic + "' not allowed.");
+            }
 
             map.put("success", true);
         } catch (IllegalArgumentException e) {
@@ -82,18 +93,6 @@ public class BrokerController {
             LOG.error(e.getMessage(), e);
         }
         return GsonUtil.toJson(map);
-    }
-
-    private List<ParamVO> parse(String queryString) {
-        queryString = queryString.trim();
-        String[] pairs = StringUtils.split(queryString, '&');
-        if (pairs != null && pairs.length > 0) {
-            for (String pair : pairs) {
-                StringUtils.split(pair, '=');
-            }
-        }
-
-        return null;
     }
 
 }
