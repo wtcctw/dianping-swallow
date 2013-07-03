242979b34c5b15a2ce05574013d791c4ad559f80
atellwu <atell@qq.com>
2013-07-03 18:58:50 +0800

    更新report脚本

--- swallow-example/src/main/java/com/dianping/swallow/example/report/ReportMaker.java
+++ swallow-example/src/main/java/com/dianping/swallow/example/report/ReportMaker.java
@@ -1,5 +1,6 @@
 package com.dianping.swallow.example.report;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Iterator;
@@ -20,30 +21,26 @@ import org.jsoup.select.Elements;
 
 public class ReportMaker {
 
-    static long totalIn = 0;
-    static long totalOut = 0;
+    static String  url = "http://cat.dianpingoa.com/cat/r/t";
+    static Compare c   = new Compare();
 
-    static String url = "http://cat.dianpingoa.com/cat/r/t";
-    static Compare c = new Compare();
-    static TreeSet<String> inTopics = new TreeSet<String>(c);
-    static TreeSet<String> inApps = new TreeSet<String>(c);
-    static TreeSet<String> outTopics = new TreeSet<String>(c);
-    static TreeSet<String> outApps = new TreeSet<String>(c);
-
-    static List<NameValuePair> initParams() {
+    static List<NameValuePair> initParams(int m) {
         List<NameValuePair> nvps = new ArrayList<NameValuePair>();
         nvps.add(new BasicNameValuePair("op", "history"));
         nvps.add(new BasicNameValuePair("domain", "Swallow"));
-        nvps.add(new BasicNameValuePair("date", "20130501"));
-        nvps.add(new BasicNameValuePair("startDate", "20130501"));
-        nvps.add(new BasicNameValuePair("endDate", "20130601"));
+        //        nvps.add(new BasicNameValuePair("date", "20130601"));
+        //        nvps.add(new BasicNameValuePair("startDate", "20130601"));
+        //        nvps.add(new BasicNameValuePair("endDate", "20130701"));
+        nvps.add(new BasicNameValuePair("date", "20130" + m + "01"));
+        nvps.add(new BasicNameValuePair("startDate", "20130" + m + "01"));
+        nvps.add(new BasicNameValuePair("endDate", "20130" + (m + 1) + "01"));
         nvps.add(new BasicNameValuePair("ip", "All"));
         nvps.add(new BasicNameValuePair("sort", "type"));
         nvps.add(new BasicNameValuePair("reportType", "month"));
         return nvps;
     }
 
-    static void parseIndexPage(String html) {
+    static void parseIndexPage(Result result, String html) {
         Document doc = Jsoup.parse(html);
         Elements els = doc.select("tr.odd,tr.even");
         if (els != null) {
@@ -59,16 +56,16 @@ public class ReportMaker {
                 String typeName = as.text().trim();
                 if (typeName.startsWith("In:")) {
                     String inTopic = typeName.substring(3);
-                    inTopics.add(inTopic);
+                    result.inTopics.add(inTopic);
                     String numStr = td2.text().replace(",", "");
                     long num = Long.parseLong(numStr);
-                    totalIn += num;
+                    result.totalIn += num;
                 } else if (typeName.startsWith("Out:")) {
                     String outTopic = typeName.substring(4);
-                    outTopics.add(outTopic);
+                    result.outTopics.add(outTopic);
                     String numStr = td2.text().replace(",", "");
                     long num = Long.parseLong(numStr);
-                    totalOut += num;
+                    result.totalOut += num;
                 }
             }
         }
@@ -86,7 +83,7 @@ public class ReportMaker {
                     String typeName = nodes.get(1).text().trim();
                     int index = typeName.indexOf(':');
                     if (index != -1) {
-                        String app = typeName.substring(0, index);
+                        String app = typeName.substring(3, index).trim();
                         if (!isLinshi(app)) {
                             apps.add(app);
                         }
@@ -97,36 +94,67 @@ public class ReportMaker {
     }
 
     public static void main(String[] args) throws Exception {
-        //        String str = "08eddbde-d6b8-4edb-86e9-fda36109cb31";
-        //        System.out.println(isLinshi(str));
+        //                String str = "08eddbde-d6b8-4edb-86e9-fda36109cb31";
+        //                System.out.println(isLinshi(str));
+
+        int m1 = 5;
+        System.out.println("-----------------Month " + m1 + "-----------------");
+        Result r1 = preMonth(m1);
+
+        int m2 = 6;
+        System.out.println("-----------------Month " + m2 + "-----------------");
+        Result r2 = preMonth(m2);
+
+        System.out.println("-----------------Month " + m2 + "月比" + m1 + "月多的topic-----------------");
+        TreeSet<String> r22 = new TreeSet<String>(r2.inTopics);
+        System.out.println(r22.removeAll(r1.inTopics));
+        System.out.println(r22);
+        System.out.println("-----------------Month " + m1 + "月比" + m2 + "月多的topic-----------------");
+        TreeSet<String> r11 = new TreeSet<String>(r1.inTopics);
+        System.out.println(r11.removeAll(r2.inTopics));
+        System.out.println(r11);
+    }
+
+    public static Result preMonth(int month) throws IOException {
+        Result result = new Result();
 
         //inTopics和outTopics
-        List<NameValuePair> nvps = initParams();
+        List<NameValuePair> nvps = initParams(month);
         String html = HttpClientUtil.get(url, nvps);
-        parseIndexPage(html);
-        System.out.println("total message in:" + totalIn);
-        System.out.println("total message out:" + totalOut);
+        parseIndexPage(result, html);
+        System.out.println("total message in:" + result.totalIn);
+        System.out.println("total message out:" + result.totalOut);
 
         //inApps
-        System.out.println("InTopics: " + inTopics.size() + ", " + StringUtils.join(inTopics, ','));
-        for (String inTopic : inTopics) {
-            List<NameValuePair> nvps2 = initParams();
+        System.out.println("InTopics: " + result.inTopics.size() + ", " + StringUtils.join(result.inTopics, ','));
+        for (String inTopic : result.inTopics) {
+            List<NameValuePair> nvps2 = initParams(month);
             nvps2.add(new BasicNameValuePair("type", "In:" + inTopic));
             html = HttpClientUtil.get(url, nvps2);
-            parseDetailPage(html, inApps);
+            parseDetailPage(html, result.inApps);
         }
-        System.out.println("InApps: " + inApps.size() + ", " + inApps);
+        System.out.println("InApps: " + result.inApps.size() + ", " + result.inApps);
 
         //outApps
-        System.out.println("OutTopics: " + outTopics.size() + ", " + outTopics);
-        for (String outTopic : outTopics) {
-            List<NameValuePair> nvps2 = initParams();
+        System.out.println("OutTopics: " + result.outTopics.size() + ", " + result.outTopics);
+        for (String outTopic : result.outTopics) {
+            List<NameValuePair> nvps2 = initParams(month);
             nvps2.add(new BasicNameValuePair("type", "Out:" + outTopic));
             html = HttpClientUtil.get(url, nvps2);
-            parseDetailPage(html, outApps);
+            parseDetailPage(html, result.outApps);
         }
-        System.out.println("OutApps: " + outApps.size() + ", " + outApps);
+        System.out.println("OutApps: " + result.outApps.size() + ", " + result.outApps);
 
+        return result;
+    }
+
+    static class Result {
+        long            totalIn   = 0;
+        long            totalOut  = 0;
+        TreeSet<String> inTopics  = new TreeSet<String>(c);
+        TreeSet<String> inApps    = new TreeSet<String>(c);
+        TreeSet<String> outTopics = new TreeSet<String>(c);
+        TreeSet<String> outApps   = new TreeSet<String>(c);
     }
 
     static class Compare implements Comparator<String> {
