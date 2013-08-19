c2e43d2f44a5b2d8b9446e39f56a21aaf5601013
kezhu.wu <atell@qq.com>
2013-08-19 17:34:41 +0800

    修复producer shell的bug

--- swallow-producerserver/src/main/resources/swallow.sh
+++ swallow-producerserver/src/main/resources/swallow.sh
@@ -77,7 +77,17 @@ if [ $PidCount -ge 1 ]; then
     exit 1
 fi
 echo "========================= swallow starting =========================="
-sh start.sh
+if [ ! -d "/data/applogs/swallow" ] ; then
+  mkdir -p "/data/applogs/swallow"
+fi
+
+JAVA_OPTS="-cp ${PRGDIR}/.:${PRGDIR}/* -server -Xms512m -Xmx2g -XX:+HeapDumpOnOutOfMemoryError -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9013 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:/data/applogs/swallow/swallow-producerserver-gc.log"
+MAIN_CLASS="com.dianping.swallow.producerserver.bootstrap.ProducerServerBootstrap"
+
+STD_OUT="/data/applogs/swallow/swallow-producerserver-std.out"
+echo "starting..."
+echo "output: $STD_OUT"
+exec java $JAVA_OPTS $MAIN_CLASS > "$STD_OUT" 2>&1 &
 
 ###########  检查是否启动成功 ############
 SuccessLog="Producer service for client is ready"
