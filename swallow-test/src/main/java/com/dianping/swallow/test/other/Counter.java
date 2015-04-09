5cfe49faca5e22817641b6f9ae5f0ce7a71d58cd
wenchao.meng <wenchao.meng@dianping.com>
2015-04-09 16:37:14 +0800

    新客户端兼容老server

---
+++ swallow-test/src/main/java/com/dianping/swallow/test/other/Counter.java
@@ -0,0 +1,39 @@
+package com.dianping.swallow.test.other;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Counter {
+
+	public static void main(String[] args) throws IOException {
+		
+		String file = "/data/applogs/swallow/all";
+		File f = new File(file);
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
+		String line = null;
+		int count[] = new int[10000];
+		int max = 0;
+		while((line = br.readLine()) != null){
+			
+			String []sp = line.split("\\s+");
+			int data = Integer.parseInt(sp[sp.length - 1]);
+			count[data]++;
+			if(data > max){
+				max = data;
+			}
+			
+		}
+
+		for(int i=0;i<=max;i++){
+			if(count[i] != 1){
+				System.out.println(i + ":" + count[i]);
+			}
+		}
+
+	}
+
+}
