a2d24dbc23551266022e68b5b1459748a9ea0415
wenchao.meng <wenchao.meng@dianping.com>
2015-05-17 10:15:55 +0800

    默认延时10s

---
+++ swallow-test/src/main/java/com/dianping/swallow/test/other/SimpleTester.java
@@ -0,0 +1,28 @@
+package com.dianping.swallow.test.other;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+/**
+ * @author mengwenchao
+ *
+ * 2015年5月16日 上午11:40:37
+ */
+public class SimpleTester {
+
+	@Test
+	public void testDate(){
+		
+//		printTime(286349333);
+		
+		printTime(286349333);
+		
+	}
+
+	private void printTime(int time) {
+		
+		System.out.println(new Date((long)time * 5 * 1000));
+		
+	}
+}
