6704506b04fee5bd309f254571a295943917cd38
lmdyyh <lmdyyh@gmail.com>
2015-06-29 15:34:52 +0800

    下载用户刷新页面后扫面任务状态

--- swallow-web/src/main/webapp/static/js/swallow-downloadfile.js
+++ swallow-web/src/main/webapp/static/js/swallow-downloadfile.js
@@ -200,6 +200,7 @@ module.controller('DownloadController', ['$rootScope', '$scope', '$http', 'Pagin
 		$scope.Timer = $interval( function(){ $scope.updatefilestatus(); }, 3000);
 	}else{
 		$scope.searchPaginator = Paginator(fetchFunction, $scope.topicnum, $scope.topic);
+		$scope.Timer = $interval( function(){ $scope.updatefilestatus(); }, 3000);
 	}
 
 			
