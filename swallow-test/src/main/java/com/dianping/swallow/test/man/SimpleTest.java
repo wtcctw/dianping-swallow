0a8a5e0860c9c7bdcde9e78b983e4971cf0492a0
wenchao.meng <wenchao.meng@dianping.com>
2015-07-09 15:56:50 +0800

    alpha环境重发处理

---
+++ swallow-test/src/main/java/com/dianping/swallow/test/man/SimpleTest.java
@@ -0,0 +1,18 @@
+package com.dianping.swallow.test.man;
+
+import com.dianping.swallow.common.internal.util.EnvUtil;
+
+/**
+ * @author mengwenchao
+ *
+ * 2015年7月9日 下午3:28:17
+ */
+public class SimpleTest {
+	
+	
+	public static void main(String []argc){
+		
+		System.out.println(EnvUtil.isProduct());
+	}
+
+}
