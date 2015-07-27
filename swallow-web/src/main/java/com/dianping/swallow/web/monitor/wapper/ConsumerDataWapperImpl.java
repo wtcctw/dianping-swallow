5ed2c83ceb1a0dc0710278b5afa225ba04667ed6
qi.yin <qi.yin@dianping.com>
2015-07-27 21:03:57 +0800

    print log ip,

--- swallow-web/src/main/java/com/dianping/swallow/web/monitor/wapper/ConsumerDataWapperImpl.java
+++ swallow-web/src/main/java/com/dianping/swallow/web/monitor/wapper/ConsumerDataWapperImpl.java
@@ -79,9 +79,9 @@ public class ConsumerDataWapperImpl extends AbstractDataWapper implements Consum
 			machineStatsData.setTimeKey(timeKey);
 			machineStatsData.setIp(serverIp);
 			ConsumerBaseStatsData baseStatsData = new ConsumerBaseStatsData();
-			
-			logger.info("[getServerStatsData]" + sendQpx.toString() + "  " + timeKey);
-			
+
+			logger.info("[getServerStatsData]" + serverIp + "   " + sendQpx.toString() + "  " + timeKey);
+
 			baseStatsData.setSendQpx(sendQpx.get(timeKey));
 			Long ackQpxValue = ackQpx.get(timeKey);
 			if (ackQpxValue != null) {
