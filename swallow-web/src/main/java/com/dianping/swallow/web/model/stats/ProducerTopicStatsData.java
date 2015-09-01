e0cd7f6e2011a0d1bdc520952cb323dcaf1b1014
qi.yin <qi.yin@dianping.com>
2015-09-01 17:30:43 +0800

    delete index

--- swallow-web/src/main/java/com/dianping/swallow/web/model/stats/ProducerTopicStatsData.java
+++ swallow-web/src/main/java/com/dianping/swallow/web/model/stats/ProducerTopicStatsData.java
@@ -4,7 +4,6 @@ import java.util.Date;
 
 import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.index.CompoundIndexes;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.dianping.swallow.web.model.event.EventType;
@@ -20,7 +19,6 @@ import com.dianping.swallow.web.model.event.StatisType;
 @CompoundIndexes({ @CompoundIndex(name = "IX_TIMEKEY_TOPICNAME", def = "{'timeKey': 1, 'topicName': -1}") })
 public class ProducerTopicStatsData extends ProducerStatsData {
 
-	@Indexed(name = "IX_TOPICNAME")
 	private String topicName;
 
 	private long totalQps;
