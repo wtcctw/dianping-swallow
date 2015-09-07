7a2ebb3c34aece1d630333e0dce94cd14fae7d96
wenchao.meng <wenchao.meng@dianping.com>
2015-09-07 11:53:37 +0800

    测试，检查消息是否重合

--- swallow-test/src/main/java/com/dianping/swallow/test/load/consumer/ConsumerRunner.java
+++ swallow-test/src/main/java/com/dianping/swallow/test/load/consumer/ConsumerRunner.java
@@ -4,8 +4,12 @@ package com.dianping.swallow.test.load.consumer;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
 
-import com.dianping.swallow.common.internal.netty.DirectBuffPrinter;
+import com.dianping.swallow.common.consumer.MessageFilter;
+import com.dianping.swallow.common.internal.util.PropertiesUtils;
 import com.dianping.swallow.common.message.Destination;
 import com.dianping.swallow.common.message.Message;
 import com.dianping.swallow.consumer.Consumer;
@@ -13,20 +17,24 @@ import com.dianping.swallow.consumer.ConsumerConfig;
 import com.dianping.swallow.consumer.MessageListener;
 import com.dianping.swallow.consumer.impl.ConsumerFactoryImpl;
 import com.dianping.swallow.test.load.AbstractLoadTest;
+import com.dianping.swallow.test.load.BitMarker;
 
 /**
- * @rundemo_name 生产者例子(同步)
+ * @author mengwenchao
+ *
+ * 2015年9月7日 上午11:45:35
  */
 public class ConsumerRunner extends AbstractLoadTest{
 
-    private static int topicCount    = 5;
+	private static int topicCount    = 2;
     private static int consumerCount = 10;
     private static int threadPoolSize = 2;
     private static int totalMessageCount = -1;
     
+    private Map<String, BitMarker> messageCount = new ConcurrentHashMap<String, BitMarker>(); 
     private static boolean differentConsumerId = true;
     
-    private String consumerIdPrefix = System.getProperty("consumerIdPrefix");
+    private String consumerIdPrefix = PropertiesUtils.getProperty("consumerIdPrefix", "myid");
     
     public static void main(String[] args) throws Exception {
     	
@@ -48,57 +56,67 @@ public class ConsumerRunner extends AbstractLoadTest{
     }
 
     @Override
-	protected void doStart() throws Exception {
+	protected void doStart() {
     	if(logger.isInfoEnabled()){
     		logger.info("[doStart][topicCount, consumerCount, threadPoolSize, totalMessageCount, differentConsumerId]" + 
     				topicCount + "," + consumerCount + "," + threadPoolSize + "," + totalMessageCount + "," + differentConsumerId);
     	}
-    	startDirectPrinter();
 		startReceiver();
 	}
-    
-	private void startDirectPrinter() throws Exception {
-		
-    	DirectBuffPrinter dbp = new DirectBuffPrinter();
-    	dbp.initialize();
-    	dbp.start();
-	}
-
-	@Override
+    @Override
 	protected boolean isExitOnExecutorsReturn() {
     	
 		return false;
 	}
-
-
-	@SuppressWarnings("deprecation")
+    @SuppressWarnings("deprecation")
 	private void startReceiver() {
 
         String rawConsumerId = getConsumerId();
 
         for (int i = 0; i < topicCount; i++) {
-            String topic = getTopicName(topicName, i);
+            final String topic = getTopicName(topicName, i);
             for (int j = 0; j < consumerCount; j++) {
                 ConsumerConfig config = new ConsumerConfig();
-                //以下两项根据自己情况而定，默认是不需要配的
+                if(j == (consumerCount - 1)){
+                	config.setMessageFilter(MessageFilter.createInSetMessageFilter(type));
+                }
                 config.setThreadPoolSize(threadPoolSize);
                 config.setRetryCountOnBackoutMessageException(0);
                 
-                String consumerId = rawConsumerId;
-                if(differentConsumerId){
-                	consumerId += "-" + j;
-                }
+                final String consumerId = differentConsumerId? (rawConsumerId + "-" + j): rawConsumerId;
                 Consumer c = ConsumerFactoryImpl.getInstance().createConsumer(Destination.topic(topic), consumerId, config);
                 c.setListener(new MessageListener() {
+                	
+                	BitMarker bitMarker = getBitMarker(topic, consumerId);
+                	
                     @Override
                     public void onMessage(Message msg) {
                     	
+                    	count(msg, bitMarker);
                     	count.incrementAndGet();
                     }
                 });
                 c.start();
             }
         }
+	}
+
+
+	private synchronized BitMarker getBitMarker(String topic, String consumerId) {
+		
+		String key = "topic-" + consumerId;
+		BitMarker bitMarker = messageCount.get(key);
+		
+		if(bitMarker == null){
+			bitMarker = new BitMarker();
+			messageCount.put(key, bitMarker);
+		}	return bitMarker;
+	}
+
+	private void count(Message msg, BitMarker bm) {
+
+		long count = Long.parseLong(msg.getContent().split(";")[0]);
+		bm.mark(count);
 	}
 
 
@@ -121,5 +139,15 @@ public class ConsumerRunner extends AbstractLoadTest{
 		}
 		
 		return false;
+	}
+	
+	@Override
+	protected void doOnExit() {
+
+		for(Entry<String, BitMarker> entry : messageCount.entrySet()){
+
+			BitMarker bitMarker = entry.getValue();
+			logger.info("[doOnExit]" + entry.getKey() + "," + bitMarker.realCount() + "," + bitMarker.noRepetCount());
+		}
 	}
 }
