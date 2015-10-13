c4768392e4ce5043613513f634c9735ef6846bd0
qi.yin <qi.yin@dianping.com>
2015-10-13 18:14:42 +0800

    add storager.

--- swallow-web/src/main/java/com/dianping/swallow/web/alarmer/storager/AbstractConsumerStatsDataStorager.java
+++ swallow-web/src/main/java/com/dianping/swallow/web/alarmer/storager/AbstractConsumerStatsDataStorager.java
@@ -5,6 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.dianping.swallow.web.monitor.ConsumerDataRetriever;
 import com.dianping.swallow.web.monitor.wapper.ConsumerStatsDataWapper;
 
+/**
+ * 
+ * @author qiyin
+ *
+ *         2015年10月13日 下午3:53:50
+ */
 public abstract class AbstractConsumerStatsDataStorager extends AbstractStatsDataStorager {
 
 	@Autowired
