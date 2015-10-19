653b4f870dc0792c6fc01f98825030767d063796
lmdyyh <lmdyyh@gmail.com>
2015-10-19 20:18:23 +0800

    删除不用的文件

--- swallow-web/src/main/java/com/dianping/swallow/web/servlet/FileDownloadServlet.java
+++ swallow-web/src/main/java/com/dianping/swallow/web/servlet/FileDownloadServlet.java
@@ -14,33 +14,33 @@ import com.dianping.swallow.web.controller.MessageDumpController;
 
 /**
  * @author mingdongli
- *
+ *         <p/>
  *         2015年6月16日下午1:40:42
  */
 public class FileDownloadServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Override
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
 
-		String filename = request.getParameter("filename");
+        String filename = request.getParameter("filename");
 
-		response.setContentType(getServletContext().getMimeType(filename));
-		response.setHeader("Content-Disposition", "attachment;filename="
-				+ filename);
-		response.setContentType("text/html;charset=utf-8");
-		String fullFileName = MessageDumpController.FILEPATH + filename;
-		InputStream in = new FileInputStream(fullFileName);
-		OutputStream out = response.getOutputStream();
+        response.setContentType(getServletContext().getMimeType(filename));
+        response.setHeader("Content-Disposition", "attachment;filename="
+                + filename);
+        response.setContentType("text/html;charset=utf-8");
+        String fullFileName = MessageDumpController.FILEPATH + filename;
+        InputStream in = new FileInputStream(fullFileName);
+        OutputStream out = response.getOutputStream();
 
-		int b;
-		while ((b = in.read()) != -1) {
-			out.write(b);
-		}
+        int b;
+        while ((b = in.read()) != -1) {
+            out.write(b);
+        }
 
-		in.close();
-		out.close();
-	}
+        in.close();
+        out.close();
+    }
 
 }
