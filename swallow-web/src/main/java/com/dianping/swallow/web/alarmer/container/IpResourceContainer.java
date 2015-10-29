58db11551d91335b4fbce3476a43a8651e5c968e
qi.yin <qi.yin@dianping.com>
2015-10-29 10:16:46 +0800

    update producerip alarm

---
+++ swallow-web/src/main/java/com/dianping/swallow/web/alarmer/container/IpResourceContainer.java
@@ -0,0 +1,8 @@
+package com.dianping.swallow.web.alarmer.container;
+
+/**
+ * @author qi.yin
+ *         2015/10/28  下午5:58.
+ */
+public interface IpResourceContainer {
+}
---
+++ swallow-web/src/main/java/com/dianping/swallow/web/alarmer/container/IpResourceContainerImpl.java
@@ -0,0 +1,8 @@
+package com.dianping.swallow.web.alarmer.container;
+
+/**
+ * @author qi.yin
+ *         2015/10/28  下午5:59.
+ */
+public class IpResourceContainerImpl {
+}
