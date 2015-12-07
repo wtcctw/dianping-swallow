6a0f395b6d9b1e324c5868f4b02e133543a8bfe0
qi.yin <qi.yin@dianping.com>
2015-12-07 19:30:34 +0800

    fix mongo config event

--- swallow-web/src/main/java/com/dianping/swallow/web/model/event/MongoConfigEvent.java
+++ swallow-web/src/main/java/com/dianping/swallow/web/model/event/MongoConfigEvent.java
@@ -6,47 +6,58 @@ import com.dianping.swallow.web.model.alarm.AlarmMeta;
 import com.dianping.swallow.web.model.alarm.AlarmType;
 import com.dianping.swallow.web.util.DateUtil;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class MongoConfigEvent extends ServerEvent {
 
-	private String topicName;
+    private static final Map<String, AlarmRecord> lastAlarms = new ConcurrentHashMap<String, AlarmRecord>();
 
-	public String getTopicName() {
-		return topicName;
-	}
+    private String topicName;
 
-	public MongoConfigEvent setTopicName(String topicName) {
-		this.topicName = topicName;
-		return this;
-	}
+    public String getTopicName() {
+        return topicName;
+    }
 
-	@Override
-	public String getMessage(String template) {
-		String message = template;
-		if (StringUtils.isNotBlank(message)) {
-			message = StringUtils.replace(message, AlarmMeta.IP_TEMPLATE, getIp());
-			message = StringUtils.replace(message, AlarmMeta.TOPIC_TEMPLATE, getTopicName());
-			message = StringUtils.replace(message, AlarmMeta.DATE_TEMPLATE, DateUtil.getDefaulFormat());
-		}
-		return message;
-	}
+    public MongoConfigEvent setTopicName(String topicName) {
+        this.topicName = topicName;
+        return this;
+    }
 
-	@Override
-	public void alarm() {
-		switch (getServerType()) {
-		case MONGO_CONFIG:
-			sendMessage(AlarmType.SERVER_MONGO_CONFIG);
-			break;
-		case MONGO_CONFIG_OK:
-			sendMessage(AlarmType.SERVER_MONGO_CONFIG_OK);
-			break;
-		default:
-			break;
-		}
-	}
+    @Override
+    public String getMessage(String template) {
+        String message = template;
+        if (StringUtils.isNotBlank(message)) {
+            message = StringUtils.replace(message, AlarmMeta.IP_TEMPLATE, getIp());
+            message = StringUtils.replace(message, AlarmMeta.TOPIC_TEMPLATE, getTopicName());
+            message = StringUtils.replace(message, AlarmMeta.DATE_TEMPLATE, DateUtil.getDefaulFormat());
+        }
+        return message;
+    }
 
-	@Override
-	public String toString() {
-		return "MongoConfigEvent [topicName=" + topicName + super.toString() + "]";
-	}
+    @Override
+    public void alarm() {
+        switch (getServerType()) {
+            case MONGO_CONFIG:
+                sendMessage(AlarmType.SERVER_MONGO_CONFIG);
+                break;
+            case MONGO_CONFIG_OK:
+                sendMessage(AlarmType.SERVER_MONGO_CONFIG_OK);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public boolean isSendAlarm(AlarmType alarmType, AlarmMeta alarmMeta) {
+        String key = getIp() + KEY_SPLIT + topicName + alarmType.getNumber();
+        return isAlarm(lastAlarms, key, alarmMeta);
+    }
+
+    @Override
+    public String toString() {
+        return "MongoConfigEvent [topicName=" + topicName + super.toString() + "]";
+    }
 
 }
