324dcbd051e5cace5902f1aa085772f392b57a44
qi.yin <qi.yin@dianping.com>
2015-12-08 13:50:20 +0800

    slave check.

--- swallow-web/src/main/java/com/dianping/swallow/web/model/server/ConsumerSlaveServer.java
+++ swallow-web/src/main/java/com/dianping/swallow/web/model/server/ConsumerSlaveServer.java
@@ -6,45 +6,42 @@ import com.dianping.swallow.web.model.event.ServerType;
 import com.dianping.swallow.web.service.HttpService.HttpResult;
 
 /**
- * 
  * @author qiyin
- *
+ *         <p/>
  *         2015年10月16日 下午3:41:24
  */
 public class ConsumerSlaveServer extends ConsumerServer {
 
-	private static final String MONOGO_MONITOR_SIGN = "mongoManager";
+    private boolean isServiceLastAlarmed = false;
 
-	private boolean isServiceLastAlarmed = false;
+    private String slaveMonitorUrl = "http://{ip}:8080/names";
 
-	private String slaveMonitorUrl = "http://{ip}:8080/names";
+    public ConsumerSlaveServer(String ip, int port) {
+        super(ip, port);
+        isServiceLastAlarmed = false;
+    }
 
-	public ConsumerSlaveServer(String ip, int port) {
-		super(ip, port);
-		isServiceLastAlarmed = false;
-	}
+    public void initServer() {
+        super.initServer();
+        slaveMonitorUrl = StringUtils.replace(serverConfig.getSlaveMonitorUrl(), "{ip}", ip);
+    }
 
-	public void initServer() {
-		super.initServer();
-		slaveMonitorUrl = StringUtils.replace(serverConfig.getSlaveMonitorUrl(), "{ip}", ip);
-	}
+    public void checkService() {
+        HttpResult result = requestUrl(slaveMonitorUrl);
+        if (!result.isSuccess() || StringUtils.isBlank(result.getResponseBody())) {
+            report(ip, ip, ServerType.SLAVE_SERVICE);
+            isServiceLastAlarmed = true;
+        } else {
+            if (isServiceLastAlarmed) {
+                report(ip, ip, ServerType.SLAVE_SERVICE_OK);
+            }
+            isServiceLastAlarmed = false;
+        }
+    }
 
-	public void checkService() {
-		HttpResult result = requestUrl(slaveMonitorUrl);
-		if (!result.isSuccess() || !result.getResponseBody().contains(MONOGO_MONITOR_SIGN)) {
-			report(ip, ip, ServerType.SLAVE_SERVICE);
-			isServiceLastAlarmed = true;
-		} else {
-			if (isServiceLastAlarmed) {
-				report(ip, ip, ServerType.SLAVE_SERVICE_OK);
-			}
-			isServiceLastAlarmed = false;
-		}
-	}
-
-	@Override
-	public String toString() {
-		return "ConsumerSlaveServer [isServiceLastAlarmed=" + isServiceLastAlarmed + "] " + super.toString();
-	}
+    @Override
+    public String toString() {
+        return "ConsumerSlaveServer [isServiceLastAlarmed=" + isServiceLastAlarmed + "] " + super.toString();
+    }
 
 }
