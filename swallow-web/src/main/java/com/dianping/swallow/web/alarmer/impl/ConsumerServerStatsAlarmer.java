a2ea8206a54bd22ff968f13065814efe71e5c22c
qi.yin <qi.yin@dianping.com>
2016-01-06 09:39:00 +0800

    update consumerserver valley alarmer.

--- swallow-web/src/main/java/com/dianping/swallow/web/alarmer/impl/ConsumerServerStatsAlarmer.java
+++ swallow-web/src/main/java/com/dianping/swallow/web/alarmer/impl/ConsumerServerStatsAlarmer.java
@@ -14,84 +14,95 @@ import com.dianping.swallow.web.monitor.wapper.ConsumerStatsDataWapper;
 import com.dianping.swallow.web.service.ConsumerServerStatsDataService;
 
 /**
- * 
  * @author qiyin
- *
+ *         <p/>
  *         2015年9月17日 下午8:24:46
  */
 @Component
 public class ConsumerServerStatsAlarmer extends AbstractStatsAlarmer {
 
-	@Autowired
-	private ConsumerDataRetriever consumerDataRetriever;
+    @Autowired
+    private ConsumerDataRetriever consumerDataRetriever;
 
-	@Autowired
-	private ConsumerStatsDataWapper consumerStatsDataWapper;
+    @Autowired
+    private ConsumerStatsDataWapper consumerStatsDataWapper;
 
-	@Autowired
-	private ConsumerServerStatsDataService serverStatsDataService;
+    @Autowired
+    private ResourceContainer resourceContainer;
 
-	@Autowired
-	private ResourceContainer resourceContainer;
+    private int ackQpsValleyCount = 0;
+    private int sendQpsValleyCount = 0;
 
-	@Override
-	public void doInitialize() throws Exception {
-		super.doInitialize();
-		consumerDataRetriever.registerListener(this);
-	}
+    @Override
+    public void doInitialize() throws Exception {
+        super.doInitialize();
+        consumerDataRetriever.registerListener(this);
+    }
 
-	@Override
-	public void doAlarm() {
-		serverAlarm();
-	}
+    @Override
+    public void doAlarm() {
+        serverAlarm();
+    }
 
-	private void serverAlarm() {
-		List<ConsumerServerStatsData> serverStatsDatas = consumerStatsDataWapper.getServerStatsDatas(getLastTimeKey(),
-				false);
-		if (serverStatsDatas == null) {
-			return;
-		}
-		for (ConsumerServerStatsData serverStatsData : serverStatsDatas) {
-			try {
-				String ip = serverStatsData.getIp();
-				ConsumerServerResource cServerResource = resourceContainer.findConsumerServerResource(ip);
-				if (cServerResource == null || !cServerResource.isAlarm()) {
-					continue;
-				}
-				QPSAlarmSetting sendQps = cServerResource.getSendAlarmSetting();
-				QPSAlarmSetting ackQps = cServerResource.getAckAlarmSetting();
+    private void serverAlarm() {
+        List<ConsumerServerStatsData> serverStatsDatas = consumerStatsDataWapper.getServerStatsDatas(getLastTimeKey(),
+                false);
+        if (serverStatsDatas == null) {
+            return;
+        }
+        for (ConsumerServerStatsData serverStatsData : serverStatsDatas) {
+            try {
+                String ip = serverStatsData.getIp();
+                ConsumerServerResource cServerResource = resourceContainer.findConsumerServerResource(ip);
+                if (cServerResource == null || !cServerResource.isAlarm()) {
+                    continue;
+                }
+                QPSAlarmSetting sendQps = cServerResource.getSendAlarmSetting();
+                QPSAlarmSetting ackQps = cServerResource.getAckAlarmSetting();
 
-				qpsSendAlarm(serverStatsData, sendQps);
-				qpsAckAlarm(serverStatsData, ackQps);
+                qpsSendAlarm(serverStatsData, sendQps);
+                qpsAckAlarm(serverStatsData, ackQps);
 
-			} catch (Exception e) {
-				logger.error("[serverAlarm] serverStatsData {} error.", serverStatsData);
-			}
-		}
-	}
+            } catch (Exception e) {
+                logger.error("[serverAlarm] serverStatsData {} error.", serverStatsData);
+            }
+        }
+    }
 
-	private boolean qpsSendAlarm(ConsumerServerStatsData serverStatsData, QPSAlarmSetting qps) {
-		if (qps != null) {
-			if (!serverStatsData.checkSendQpsPeak(qps.getPeak())) {
-				return false;
-			}
-			if (!serverStatsData.checkSendQpsValley(qps.getValley())) {
-				return false;
-			}
-		}
-		return true;
-	}
+    private boolean qpsSendAlarm(ConsumerServerStatsData serverStatsData, QPSAlarmSetting qps) {
+        if (qps != null) {
+            if (!serverStatsData.checkSendQpsPeak(qps.getPeak())) {
+                sendQpsValleyCount = 0;
+                return false;
+            }
+            if (!serverStatsData.checkSendQpsValley(qps.getValley())) {
+                sendQpsValleyCount++;
+                if (sendQpsValleyCount > 3) {
+                    return false;
+                }
+                return true;
+            }
+        }
+        sendQpsValleyCount = 0;
+        return true;
+    }
 
-	private boolean qpsAckAlarm(ConsumerServerStatsData serverStatsData, QPSAlarmSetting qps) {
-		if (qps != null) {
-			if (!serverStatsData.checkAckQpsPeak(qps.getPeak())) {
-				return false;
-			}
-			if (!serverStatsData.checkAckQpsValley(qps.getValley())) {
-				return false;
-			}
-		}
-		return true;
-	}
+    private boolean qpsAckAlarm(ConsumerServerStatsData serverStatsData, QPSAlarmSetting qps) {
+        if (qps != null) {
+            if (!serverStatsData.checkAckQpsPeak(qps.getPeak())) {
+                ackQpsValleyCount = 0;
+                return false;
+            }
+            if (!serverStatsData.checkAckQpsValley(qps.getValley())) {
+                ackQpsValleyCount++;
+                if (ackQpsValleyCount > 3) {
+                    return false;
+                }
+                return true;
+            }
+        }
+        ackQpsValleyCount = 0;
+        return true;
+    }
 
 }
--- swallow-web/src/main/java/com/dianping/swallow/web/alarmer/impl/ProducerServerStatsAlarmer.java
+++ swallow-web/src/main/java/com/dianping/swallow/web/alarmer/impl/ProducerServerStatsAlarmer.java
@@ -14,68 +14,72 @@ import com.dianping.swallow.web.monitor.wapper.ProducerStatsDataWapper;
 import com.dianping.swallow.web.service.ProducerServerStatsDataService;
 
 /**
- * 
  * @author qiyin
- *
+ *         <p/>
  *         2015年8月3日 下午6:06:54
  */
 @Component
 public class ProducerServerStatsAlarmer extends AbstractStatsAlarmer {
 
-	@Autowired
-	private ProducerDataRetriever producerDataRetriever;
+    @Autowired
+    private ProducerDataRetriever producerDataRetriever;
 
-	@Autowired
-	private ProducerStatsDataWapper producerStatsDataWapper;
+    @Autowired
+    private ProducerStatsDataWapper producerStatsDataWapper;
 
-	@Autowired
-	private ProducerServerStatsDataService serverStatsDataService;
+    @Autowired
+    private ResourceContainer resourceContainer;
 
-	@Autowired
-	private ResourceContainer resourceContainer;
+    private int qpsValleyCount = 0;
 
-	@Override
-	public void doInitialize() throws Exception {
-		super.doInitialize();
-		producerDataRetriever.registerListener(this);
-	}
+    @Override
+    public void doInitialize() throws Exception {
+        super.doInitialize();
+        producerDataRetriever.registerListener(this);
+    }
 
-	@Override
-	public void doAlarm() {
-		serverAlarm();
-	}
+    @Override
+    public void doAlarm() {
+        serverAlarm();
+    }
 
-	private void serverAlarm() {
-		List<ProducerServerStatsData> serverStatsDatas = producerStatsDataWapper.getServerStatsDatas(getLastTimeKey(),
-				false);
-		if (serverStatsDatas == null || serverStatsDatas.isEmpty()) {
-			return;
-		}
-		for (ProducerServerStatsData serverStatsData : serverStatsDatas) {
-			try {
-				String ip = serverStatsData.getIp();
-				ProducerServerResource pServerResource = resourceContainer.findProducerServerResource(ip);
-				if (pServerResource == null || !pServerResource.isAlarm()) {
-					continue;
-				}
-				QPSAlarmSetting qps = pServerResource.getSaveAlarmSetting();
-				qpsAlarm(serverStatsData, qps);
-			} catch (Exception e) {
-				logger.error("[serverAlarm] serverStatsData {} error.", serverStatsData);
-			}
-		}
-	}
+    private void serverAlarm() {
+        List<ProducerServerStatsData> serverStatsDatas = producerStatsDataWapper.getServerStatsDatas(getLastTimeKey(),
+                false);
+        if (serverStatsDatas == null || serverStatsDatas.isEmpty()) {
+            return;
+        }
+        for (ProducerServerStatsData serverStatsData : serverStatsDatas) {
+            try {
+                String ip = serverStatsData.getIp();
+                ProducerServerResource pServerResource = resourceContainer.findProducerServerResource(ip);
+                if (pServerResource == null || !pServerResource.isAlarm()) {
+                    continue;
+                }
+                QPSAlarmSetting qps = pServerResource.getSaveAlarmSetting();
+                qpsAlarm(serverStatsData, qps);
+            } catch (Exception e) {
+                logger.error("[serverAlarm] serverStatsData {} error.", serverStatsData);
+            }
+        }
+    }
 
-	private boolean qpsAlarm(ProducerServerStatsData serverStatsData, QPSAlarmSetting qps) {
-		if (qps != null) {
-			if (!serverStatsData.checkQpsPeak(qps.getPeak())) {
-				return false;
-			}
-			if (!serverStatsData.checkQpsValley(qps.getValley())) {
-				return false;
-			}
-		}
-		return true;
-	}
+    private boolean qpsAlarm(ProducerServerStatsData serverStatsData, QPSAlarmSetting qps) {
+        if (qps != null) {
+            if (!serverStatsData.checkQpsPeak(qps.getPeak())) {
+                qpsValleyCount = 0;
+                return false;
+            }
+            if (!serverStatsData.checkQpsValley(qps.getValley())) {
+                qpsValleyCount++;
+                if (qpsValleyCount > 3) {
+                    return false;
+                }
+                return true;
+            }
+        }
+        qpsValleyCount = 0;
+        return true;
+    }
 
 }
