8ea6737ad31f2445d077d3f96ecd491b2cf840d4
lmdyyh <lmdyyh@gmail.com>
2016-01-28 13:05:51 +0800

    优雅的关闭broker

--- bin/kafka/kafka_2.9.1-0.8.2.2/bin/swallow_start.sh
+++ bin/kafka/kafka_2.9.1-0.8.2.2/bin/swallow_start.sh
@@ -13,6 +13,30 @@ export LOG_DIR=/data/applogs/kafka
 export KAFKA_HEAP_OPTS="-Xms4g -Xmx4g -XX:PermSize=48m -XX:MaxPermSize=48m"
 export KAFKA_JVM_PERFORMANCE_OPTS="-XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 -XX:+DisableExplicitGC -Djava.awt.headless=true"
 
-jps | grep -w "Kafka" | awk '{print $1}' | xargs kill -9 2>/dev/null
+
+kpid=`jps | grep -w "Kafka" | awk '{print $1}'` 
+
+if [ "$kpid" != "" ]; then
+
+    before=$(($(date +%s%N)/1000000))
+
+    kill -SIGTERM $kpid &
+    wait $!
+    echo "kill kafka server with pid $kpid"
+
+    middle=$(($(date +%s%N)/1000000))
+    echo "kill kafka server with "$(($middle-$before))" ms"
+
+    closed=`netstat -ant|grep 9092 | wc -l`
+    while [ "$closed" -ne 0 ]
+    do
+        sleep 0.01
+        closed=`netstat -ant|grep 9092 | wc -l`
+    done
+
+    after=$(($(date +%s%N)/1000000))
+
+    echo "connection all closed after "$(($after-$middle))" ms"
+fi
 
 $base_dir/kafka-server-start.sh -daemon $base_dir/../config/server.properties
