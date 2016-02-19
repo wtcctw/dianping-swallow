a3a9bd688b2cf430dffc7fefcf0b4d61cf48b959
lmdyyh <lmdyyh@gmail.com>
2016-02-19 16:45:37 +0800

    重命名ReportableKafkaJmx.java

---
+++ swallow-web/src/main/java/com/dianping/swallow/web/monitor/jmx/ReportableKafkaJmx.java
@@ -0,0 +1,10 @@
+package com.dianping.swallow.web.monitor.jmx;
+
+import com.dianping.swallow.web.alarmer.EventReporter;
+
+/**
+ * Author   mingdongli
+ * 16/2/19  下午4:44.
+ */
+public interface ReportableKafkaJmx extends KafkaJmx, EventReporter {
+}
